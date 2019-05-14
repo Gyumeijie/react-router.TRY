00a427efa4fb410f368ac12a12174b0969065e8c
Gyumeijie <18170879819@163.com>
2019-05-14 14:45:39 +0800

    Part III
    recursive paths, to the infinity and beyond

--- examples/simple-personal-website/src/index.js
+++ examples/simple-personal-website/src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Routes } from './routes'; // where we are going to specify our routes
-import registerServiceWorker from './registerServiceWorker';
+// 'React' must be in scope when using JSX
+import React from "react";
+import ReactDOM from "react-dom";
+
+/*<BrowserRouter>: A <Router> that uses the HTML5 history API 
+(pushState, replaceState and the popstate event) to keep your 
+ UI in sync with the URL.*/
+ import { BrowserRouter as Router } from "react-router-dom";
+
+ /*<HashRouter>: A <Router> that uses the hash portion of the URL 
+ i.e. window.location.hash to keep your UI in sync with the URL.*/
+
+
+// import { HashRouter as Router } from "react-router-dom";
+
+// where we are going to specify our routes
+import { Routes } from "./routes";
+import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
   <Router>
     <Routes />
   </Router>,
-  document.getElementById('root'),
+  document.getElementById("root")
 );
 
 registerServiceWorker();
